154aee3be66b15dc1e55d00302061e93400d48ce
Akshay Rathod <workforakshay@gmail.com>
2023-12-18 20:25:37 +0530

    Add project files.

---
+++ BookWeb(MVC)/Data/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using BookWeb_MVC_.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+
+namespace BookWeb_MVC_.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Catagory> Categories { get; set; }
+    }
+}
